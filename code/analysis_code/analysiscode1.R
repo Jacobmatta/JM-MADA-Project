b2221594ec46d0528ac989d413287c526efc4d49
Jacob Matta <jpm86668@uga.edu>
2023-03-13 12:04:17 -0400

    analysis code 1
    started writing code for analysis 1 where I intend to make visual for the trend of vax from sept to feb

--- code/analysis_code/analysiscode1.R
+++ code/analysis_code/analysiscode1.R
@@ -9,16 +9,30 @@ library(tidyr)
 # Loading the data
 
 ## Loading September 31701
-data_location1 = here("data", "processed_data", "Sept", "processed_data_31701_sept.rds")
-prcessed_data_31701_sept = readRDS(data_location1)
+data_location1 = here("data", "processed_data", "Sept", "processed_data_31701_Sept.rds")
+processed_data_31701_Sept = readRDS(data_location1)
 
-## Loading October 31701
-data_location1 = here("data", "processed_data", "Oct", "processed_data_31701_Oct.rds")
-prcessed_data_31701_Oct = readRDS(data_location1)
+## Loading October 31701 and joining september and october 
+data_location2 = here("data", "processed_data", "Oct", "processed_data_31701_Oct.rds")
+processed_data_31701_Oct = readRDS(data_location2)
+processed_combined <- left_join(processed_data_31701_Sept, processed_data_31701_Oct, by = c("FIPS" = "FIPS"))
+
+## Loading November 31701
+data_location3 = here("data", "processed_data", "Nov", "processed_data_31701_Nov.rds")
+processed_data_31701_Nov = readRDS(data_location3)
 
 ## Loading December 31701
-data_location1 = here("data", "processed_data", "Oct", "processed_data_31701_Oct.rds")
-prcessed_data_31701_sept = readRDS(data_location1)
+data_location4 = here("data", "processed_data", "Dec", "processed_data_31701_Dec.rds")
+processed_data_31701_Dec = readRDS(data_location4)
+
+## Loading January 31701
+data_location5 = here("data", "processed_data", "Jan", "processed_data_31701_Jan.rds")
+processed_data_31701_Jan = readRDS(data_location5)
+
+## Loading February 31701
+data_location6 = here("data", "processed_data", "Feb", "processed_data_31701_Feb.rds")
+processed_data_31701_Feb = readRDS(data_location6)
+
 
 filename <- file.choose()
 Canteen_clean <- readRDS(filename)
